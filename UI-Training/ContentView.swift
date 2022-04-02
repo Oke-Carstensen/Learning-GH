b53d3f08668010d577bfe617d6b3749a0f509f5e
Oke-Carstensen <carstensenoke@icloud.com>
2022-04-02 17:20:04 +0200

    Initial Commit

---
+++ UI-Training/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  UI-Training
+//
+//  Created by Oke Carstensen on 31.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+     Text("Hello World")
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI-Training/UI_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_TrainingApp.swift
+//  UI-Training
+//
+//  Created by Oke Carstensen on 31.03.22.
+//
+
+import SwiftUI
+
+@main
+struct UI_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
